54c0aa463fb6fda4c604a51c8b4e14679a6f3dbc
Hoon H <drawtree@me.com>
2014-09-12 13:12:05 +0900

    (autocommit by a script for a checkpoint, working on...)

--- Tools/add-commit-push.bash
+++ Tools/add-commit-push.bash
@@ -2,14 +2,74 @@
 
 set -e
 
-cd Components
-cd Common
 
-BR=`git branch --list "master"`
-if [ "$BR" = "* master" ]
-then
-	echo "OK!"
-else
-	echo "Current branch is not set to 'master'."
-	exit 1
-fi
+function log
+{
+	echo "$@";
+}
+function logok
+{
+	log "[OK]" "$@";
+}
+function logerr
+{
+	echo "[ERR]" "$@" 1>&2;
+}
+
+function assert_master_branch
+{
+	logok "Current location:" `pwd`
+	BR=`git branch --list "master"`
+	if [ "$BR" = "* master" ]
+	then
+		logok "Here is a master branch."
+	else
+		logerr "Current branch is not set to 'master'."
+		exit 1
+	fi
+}
+
+function process_if_possible
+{
+	S1=`git status --short | wc -l`
+
+	if (( $S1 > 0 ))
+	then
+		logok "Something to commit. Do it."
+		git add . -A
+		git commit -am "(autocommit by a script for a checkpoint, working on...)"
+		git push
+	else
+		logok "Nothing to commit. Skipping"
+	fi
+}
+function test_and_perform
+{
+	BACK=`pwd`
+	cd $1
+	assert_master_branch
+
+	git status --short
+	process_if_possible
+	cd "$BACK"
+	log
+}
+
+# Checkup exercise.
+cd Monolith
+cd ..
+
+log
+logok "Starting..."
+log
+
+test_and_perform Components/Common
+test_and_perform Components/CommonMark
+test_and_perform Components/SQLite3
+test_and_perform Components/XML
+test_and_perform .
+
+logok "Done."
+
+
+
